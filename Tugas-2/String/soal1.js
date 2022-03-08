70bf1f9819fd834c142627eb734d0e2b1128e51f
baimsjr <baimsjr@gmail.com>
2022-03-09 00:06:32 +0700

    SOAL 1

---
+++ Tugas-2/String/soal1.js
@@ -0,0 +1,12 @@
+var word = "JavaScript" 
+var second = "is" 
+var third = "awesome" 
+var fourth = "and"
+var fifth = "I" 
+var sixth = "love" 
+var seventh = "it!"
+
+var Tugas2 = `${word} ${second} ${third} ${fourth} ${fifth} ${sixth} ${seventh}`
+console.log(Tugas2)
+
+
