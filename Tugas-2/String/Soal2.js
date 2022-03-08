3084bb851581c9503d2b666afe1abc7fff78a830
baimsjr <baimsjr@gmail.com>
2022-03-09 00:07:58 +0700

    SOAL 1

--- Tugas-2/String/Soal2.js
+++ Tugas-2/String/Soal2.js
@@ -7,15 +7,8 @@ var fourthWord = sentence[10] + sentence[11] + sentence[12]
 var fifthWord = sentence[13] + sentence[14] + sentence[15]
 var sixthWord = sentence[16] + sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21]
 var seventhWord = sentence[22] + sentence[23] +sentence[24] + sentence[25] + sentence[26] + sentence[27] + sentence[28]
-var eighthWord = sentence[29] + sentence[30] +sentence[31] + sentence[32] + sentence[33] + sentence[34] + sentence[35] + sentence[36] + sentence[37] +sentence[38]
+var eighthWord = sentence[29] + sentence[30] +sentence[31] + sentence[32] + sentence[33] + sentence[34] + sentence[35] + sentence[36] + sentence[37] +sentence[r]
 
-console.log(exampleFirstWord); 
-console.log(secondWord); 
-console.log(thirdWord); 
-console.log(fourthWord); 
-console.log(fifthWord); 
-console.log(sixthWord); 
-console.log(seventhWord); 
-console.log(eighthWord)
-
+console.log('First Word: '+ exampleFirstWord)
+console.log('Second Word: ' + secondWord)
 
